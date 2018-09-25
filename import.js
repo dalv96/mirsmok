f2e9cef2daa719e70e64c8199618e9e71e7607e1
Vladislav Telichko <vladislav.telichko@ya.ru>
2018-09-25 10:53:05 +0300

    One more fix

--- import.js
+++ import.js
@@ -86,7 +86,9 @@ var imprt = async () => {
         } else console.log('Found dublicate');
     })
 
-    popo.forEach( async (item) => {
+    for (let i = 0; i < popo.length; i++) {
+        var item = popo[i];
+
         var auth = robots[item['Ф.И.О. автора']];        
         if(!auth) console.log(item['Ф.И.О. автора']);
 
@@ -112,10 +114,10 @@ var imprt = async () => {
                     adress: item['Адрес']
                 }
             })
-            
+            var exec;
 
             if(item.type == 'install') {
-                var exec = await Exec.findOne({name: item['Монтажник']});
+                exec = await Exec.findOne({name: item['Монтажник']});
                 if (exec) {
                     order.nameExec = [exec];
                 }
@@ -125,10 +127,7 @@ var imprt = async () => {
             }
 
             if(item.type == 'remonts') {
-                var exec = await Exec.findOne({name: item['Ф.И.О. исполнителя']});
-                if (exec) {
-                    order.nameExec = [exec];
-                }
+                exec = await Exec.findOne({name: item['Ф.И.О. исполнителя']});
                 order.type = 1;
                 order.tip = item['Ф.И.О. исполнителя'];
                 order.info.numberTT = item['Номер ТТ'];
@@ -136,9 +135,64 @@ var imprt = async () => {
             }
 
             id++;
-            return order.save();
+            
+            var done = await order.save();
         }
-    })
+    }
+    // popo.forEach( async (item) => {
+    //     var auth = robots[item['Ф.И.О. автора']];        
+    //     if(!auth) console.log(item['Ф.И.О. автора']);
+
+    //     var range = getRange(item['Дата выезда']);
+    //     var test = await Order.find({
+    //         'info.nameAbon': item['Ф.И.О. абонента'],
+    //         $and: [
+    //             { 'info.dateEvent': { $gte: range[0] } },
+    //             { 'info.dateEvent': { $lte: range[1] } }
+    //         ]
+    //     });
+       
+    //     if(test.length == 0) {
+    //         var order = new Order({
+    //             id: id,
+    //             stage: 0,
+    //             author: auth,
+    //             info: {
+    //                 dateInit: new Date(),
+    //                 dateEvent: item['Дата выезда'],
+    //                 nameAbon: item['Ф.И.О. абонента'],
+    //                 phone: item['Номер телефона абонента'],
+    //                 adress: item['Адрес']
+    //             }
+    //         })
+            
+
+    //         if(item.type == 'install') {
+    //             var exec = await Exec.findOne({name: item['Монтажник']});
+    //             if (exec) {
+    //                 order.nameExec = [exec];
+    //             }
+    //             order.info.personalAcc = item['Лицевой счет'];
+    //             order.tip = item['Монтажник'];
+    //             order.type = 0;
+    //         }
+
+    //         if(item.type == 'remonts') {
+    //             var exec = await Exec.findOne({name: item['Ф.И.О. исполнителя']});
+    //             if (exec) {
+    //                 order.nameExec = [exec];
+    //             }
+    //             order.type = 1;
+    //             order.tip = item['Ф.И.О. исполнителя'];
+    //             order.info.numberTT = item['Номер ТТ'];
+    //             order.info.themeTT = item['Тема ТТ'];
+    //         }
+
+    //         id++;
+            
+    //         return order.save();
+    //     }
+    // })
 }
 
 imprt();
